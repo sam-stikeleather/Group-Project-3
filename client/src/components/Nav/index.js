6a09891fcd07d2b1364ae0276a1f13c77a00f607
C-Bolick <134232955+C-Bolick@users.noreply.github.com>
2023-08-15 10:33:23 -0400

    5

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -43,7 +43,7 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
+          <span role="img" aria-label="shopping bag">♥</span>
           -Shop-Shop
         </Link>
       </h1>
